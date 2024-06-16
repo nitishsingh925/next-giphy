74e2cd6c48eb30739f5cd426c66b579fe7e7b02d
Nitish Singh <nitishsingh925@gmail.com>
2024-06-16 11:25:16 +0530

    fix home css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Home: React.FC = () => {
   return (
-    <div className="container px-6 py-4 mx-auto">
+    <div className="">
       <Image
         src="/banner.gif"
         alt="banner"
