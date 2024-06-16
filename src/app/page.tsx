b0f45b191a6bcdae3f0fd171a4a25f469b0078dc
Nitish Singh <nitishsingh925@gmail.com>
2024-06-16 11:23:50 +0530

    add home page banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 const Home: React.FC = () => {
-  return <div className="container px-6 py-4 mx-auto">home</div>;
+  return (
+    <div className="container px-6 py-4 mx-auto">
+      <Image
+        src="/banner.gif"
+        alt="banner"
+        width={500}
+        height={500}
+        unoptimized
+        className="mt-2 rounded w-full"
+      />
+    </div>
+  );
 };
 
 export default Home;
